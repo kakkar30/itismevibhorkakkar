995507d760a2f66e49c7a0c69de01fc6556c8604
kakkar30 <62466191+kakkar30@users.noreply.github.com>
2020-03-21 17:00:50 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook would be used for my Capstone Project
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
